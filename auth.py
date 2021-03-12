21c18ea0a7f6c13a0b349343cf8c7f5d57a478d8
Scott <scott.miner.data.scientist@gmail.com>
2021-03-11 17:41:33 -0700

    final commit

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
 
@@ -33,11 +34,28 @@ def signup_post():
     
     return redirect(url_for('auth.login'))
 
-#@auth.route('/login', methods=['POST'])
-#def login_post():
+@auth.route('/login', methods=['POST'])
+def login_post():
     
-
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    
+    user = User.query.filter_by(email=email).first()
+    
+    #check if user exists
+    #take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+    
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile')) 
+    
 
 @auth.route('/logout')
 def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
     return 'Logout'
